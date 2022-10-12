44181b4cf1856674614acf2c81e34794907d23b4
Jerzy Biernacki <jerzy.biernacki@gmail.com>
2022-10-12 13:21:51 +0200

    Confirmation page with spinner and some dummy data removed

--- frontend/webapp/src/pages/Confirmation.page.tsx
+++ frontend/webapp/src/pages/Confirmation.page.tsx
@@ -1,41 +1,27 @@
-import { Button } from 'primereact/button'
 import React, { useEffect, useState } from 'react'
-
+import { Button } from 'primereact/button'
 import Card from '../components/Card'
 import Star from '../assets/img-star.svg'
+import { ProgressSpinner } from 'primereact/progressspinner'
 import './Confirmation.scss'
-import { useHeaderAccess } from '../context/HederaProvider'
-import { MessageTypes } from 'hashconnect'
-import {
-  Client,
-  ContractCallQuery,
-  ContractExecuteTransaction,
-  ContractFunctionParameters,
-  Hbar
-} from '@hashgraph/sdk'
-import HashieConfig from '../settings.json'
 import { useParams } from 'react-router-dom'
-import BigNumber from 'bignumber.js'
-import { hethers } from '@hashgraph/hethers'
 
 const Confirmation = () => {
-  const { isConnected, connect, signer, hashConnect } = useHeaderAccess()
   const { code: collectionId } = useParams()
-  const [name, setName] = useState<string>('loading...')
+  const [loading, isLoading] = useState<boolean>(true)
+  const [name, setName] = useState<string | null>(null)
   const [image, setImage] = useState<string | null>(null)
-  const [description, setDescription] = useState<string>('loading...')
+  const [description, setDescription] = useState<string | null>(null)
 
   useEffect(() => {
     if (!collectionId) return
-    if (!signer) return
-    if (!hashConnect) return
 
     const _fetch = async () => {
+      isLoading(true)
       const response = await fetch(
         `https://ipfs.io/ipfs/${collectionId}/metadata.json`
       )
       const { description, image, name } = await response.json()
-      console.log(description, image, name)
       if (image.startsWith('ipfs://')) {
         const [imageCid, imageFileName] = image
           .replace(/^ipfs:\/\//, '')
@@ -46,9 +32,12 @@ const Confirmation = () => {
       }
       setDescription(description)
       setName(name)
+      isLoading(false)
     }
-    _fetch()
-  }, [collectionId])
+    if (name === null && description === null && image === null) {
+      _fetch()
+    }
+  }, [collectionId, name, description, image])
 
   return (
     <div className="flex justify-content-center align-items-center">
@@ -58,29 +47,28 @@ const Confirmation = () => {
         ) : (
           <img src={Star} alt="Placeholder image" className="col-3" />
         )}
-        <div className="text-lg text-left text-white col-9 pl-4">{name}</div>
+        <div className="text-lg text-left text-white col-9 pl-4">
+          {name}
+          {loading && <ProgressSpinner />}
+        </div>
         <div className="tex-sm text-left col-12 mt-4">Status</div>
         <div className="tex-sm text-left text-white col-12 mt-2">Status</div>
         <div className="tex-sm text-left col-12 mt-4">Event description</div>
         <div className="tex-sm text-left text-white col-12 mt-2">
           {description}
         </div>
-        <div className="tex-sm text-left col-12 mt-4">Limited quantity</div>
-        <div className="tex-sm text-left text-white col-12 mt-2">1000</div>
-        <div className="tex-sm text-left col-12 mt-4">Time limit</div>
+        {/* <div className="tex-sm text-left col-12 mt-4">Limited quantity</div>
+        <div className="tex-sm text-left text-white col-12 mt-2">1000</div> */}
+        {/* <div className="tex-sm text-left col-12 mt-4">Time limit</div>
         <div className="tex-sm text-left text-white col-12 mt-2">
           10/10/2022 — 12/10/2022
-        </div>
-        <div className="tex-sm text-left col-12 mt-4">Secret code</div>
-        <div className="tex-sm text-left text-white col-12 mt-2">
-          &#123;CODE&#125;
-        </div>
-        <div className="tex-sm text-left col-12 mt-4">Cost</div>
-        <div className="tex-sm text-left text-white col-12 mt-2">Free</div>
+        </div> */}
+        {/* <div className="tex-sm text-left col-12 mt-4">Cost</div>
+        <div className="tex-sm text-left text-white col-12 mt-2">Free</div> */}
         <div className="tex-sm text-left col-12 mt-4">Claim URL</div>
         <div className="col-12 mb-4 flex align-items-center">
-          <a href={`https://hashies.net/mint/${collectionId}`}>
-            https://hashies.net/mint/{collectionId}
+          <a href={`${window.location.origin}/mint/${collectionId}`}>
+            {window.location.origin}/mint/{collectionId}
           </a>
           <Button icon="pi pi-copy" className="p-button-text" />
         </div>
