a794b819f8adcaa47a7a0033d95a1f4ccb945c07
MB <>
2022-10-12 16:06:36 +0200

    Webapp: Event page ui tweaks

--- frontend/webapp/src/pages/Confirmation.page.tsx
+++ frontend/webapp/src/pages/Confirmation.page.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import { Button } from 'primereact/button'
 import Card from '../components/Card'
 import Star from '../assets/img-star.svg'
 import './Confirmation.scss'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { useHeaderAccess } from '../context/HederaProvider'
 import {
   ContractExecuteTransaction,
@@ -11,7 +11,10 @@ import {
 } from '@hashgraph/sdk'
 import HashieConfig from '../settings.json'
 
+import { Toast } from 'primereact/toast'
+
 const Confirmation = () => {
+  const toast = useRef<any>()
   const { code: collectionId } = useParams()
   const [loading, isLoading] = useState<boolean>(true)
   const [name, setName] = useState<string | null>(null)
@@ -20,14 +23,15 @@ const Confirmation = () => {
   const { signer } = useHeaderAccess()
 
   useEffect(() => {
-    if (!collectionId) return
-
     const _fetch = async () => {
       isLoading(true)
       const response = await fetch(
         `https://ipfs.io/ipfs/${collectionId}/metadata.json`
       )
-      const { description, image, name } = await response.json()
+      const data = await response.json()
+      console.log(data)
+      console.log(window.location.origin)
+      const { description, image, name } = data
       if (image.startsWith('ipfs://')) {
         const [imageCid, imageFileName] = image
           .replace(/^ipfs:\/\//, '')
@@ -40,7 +44,12 @@ const Confirmation = () => {
       setName(name)
       isLoading(false)
     }
-    if (name === null && description === null && image === null) {
+    if (
+      name === null &&
+      description === null &&
+      image === null &&
+      collectionId
+    ) {
       _fetch()
     }
   }, [collectionId, name, description, image])
@@ -70,56 +79,71 @@ const Confirmation = () => {
     console.log(result)
   }
 
+  const url = useMemo(() => {
+    return `https://hashie.net/mint/${collectionId}`
+  }, [collectionId])
+
   return (
     <div className="flex justify-content-center align-items-center">
       <Card className="w-7 grid grid-nogutter">
-        {image ? (
-          <img src={image} alt="Event image" className="col-3" />
+        {!loading ? (
+          <>
+            {image ? (
+              <img src={image} alt="Event image" className="col-3" />
+            ) : (
+              <img src={Star} alt="Placeholder image" className="col-3" />
+            )}
+            {!loading && (
+              <div className="text-lg text-left text-white col-9 pl-4">
+                {name}
+              </div>
+            )}
+            <div className="text-sm text-left col-12 mt-4">
+              Event description
+            </div>
+            <div className="text-sm text-left text-white col-12 mt-2">
+              {description}
+            </div>
+            <div className="text-sm text-left col-12 mt-4 ">Claim URL</div>
+            <div className="flex justify-items-center align-items-center">
+              <Link to={'/mint/' + collectionId}>URL</Link>
+              <Button
+                icon="pi pi-copy"
+                className="p-button-text"
+                onClick={() => {
+                  navigator.clipboard.writeText(url)
+                  toast?.current?.show({
+                    severity: 'info',
+                    summary: 'URL copied to clipboard'
+                  })
+                }}
+              />
+            </div>
+            <div className="col-12 mb-4 flex flex-column align-items-center">
+              <Button
+                label="Mint Hashie!"
+                className="submit mt-4"
+                onClick={handleMint}
+              />
+            </div>
+            <div className="col-12 text-xs">
+              <p>Event id: {collectionId}</p>
+              <p>Created on 6.06.2022, 04:43:19</p>
+            </div>
+          </>
         ) : (
-          <img src={Star} alt="Placeholder image" className="col-3" />
+          <>
+            <div className="flex flex-column justify-content-center align-items-center col-12">
+              <i
+                className="pi pi-spin pi-spinner col-12"
+                style={{ fontSize: '5em', color: '#6166DC' }}
+              />
+              <div className="col-12 text-lg text-white mt-4">Loading...</div>
+            </div>
+          </>
         )}
-        {!loading && (
-          <div className="text-lg text-left text-white col-9 pl-4">{name}</div>
-        )}
-        {loading && (
-          <div className="text-lg text-left text-white col-9 pl-4">
-            <i
-              className="pi pi-spin pi-spinner col-12"
-              style={{ fontSize: '5em', color: '#6166DC' }}
-            />
-            <div className="col-12 text-sm text-white mt-4">Loading...</div>
-          </div>
-        )}
-        <div className="tex-sm text-left col-12 mt-4">Status</div>
-        <div className="tex-sm text-left text-white col-12 mt-2">Status</div>
-        <div className="tex-sm text-left col-12 mt-4">Event description</div>
-        <div className="tex-sm text-left text-white col-12 mt-2">
-          {description}
-        </div>
-        {/* <div className="tex-sm text-left col-12 mt-4">Limited quantity</div>
-        <div className="tex-sm text-left text-white col-12 mt-2">1000</div> */}
-        {/* <div className="tex-sm text-left col-12 mt-4">Time limit</div>
-        <div className="tex-sm text-left text-white col-12 mt-2">
-          10/10/2022 — 12/10/2022
-        </div> */}
-        {/* <div className="tex-sm text-left col-12 mt-4">Cost</div>
-        <div className="tex-sm text-left text-white col-12 mt-2">Free</div> */}
-        <div className="tex-sm text-left col-12 mt-4">Claim URL</div>
-        <div className="col-12 mb-4 flex flex-column align-items-center">
-          <Button
-            label="Mint Hashie!"
-            className="submit mt-4"
-            // onClick={() =>
-            //   (window.location.href = `${window.location.origin}/mint/${collectionId}`)
-            // }
-            onClick={handleMint}
-          />
-        </div>
-        <div className="col-12 text-xs">
-          <p>Event id: {collectionId}</p>
-          <p>Created on 6.06.2022, 04:43:19</p>
-        </div>
       </Card>
+      <Toast ref={toast} position="top-center" />
     </div>
   )
 }
