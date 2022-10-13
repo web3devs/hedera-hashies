f0d230aa191ed9eb25a1baa7b62b18b574eb371f
Jerzy Biernacki <jerzy.biernacki@gmail.com>
2022-10-13 16:24:22 +0200

    Homepage: Connect Wallet button instead of Create an Event if the wallet is not connected

--- frontend/webapp/src/pages/Home.page.tsx
+++ frontend/webapp/src/pages/Home.page.tsx
@@ -2,9 +2,17 @@ import { Button } from 'primereact/button'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 // import InfoCard from '../components/home/InfoCard'
+import { useHeaderAccess } from '../context/HederaProvider'
+
 import './Home.scss'
 const Home = () => {
   const navigate = useNavigate()
+  const { isConnected, connect } = useHeaderAccess()
+
+  const handleConnect = async () => {
+    connect()
+  }
+
   return (
     <div className="home">
       <div className="text-6xl text-white mb-4 font-normal">
@@ -14,11 +22,19 @@ const Home = () => {
       <div className="text mb-4">
         Create events for your communities and prove that they were there.
       </div>
-      <Button
-        label="Create an Event"
-        className="create-event mb-8"
-        onClick={() => navigate('/add-event')}
-      />
+      {isConnected ? (
+        <Button
+          label="Create an Event"
+          className="create-event mb-8"
+          onClick={() => navigate('/add-event')}
+        />
+      ) : (
+        <Button
+          label="Connect Wallet"
+          className="create-event mb-8"
+          onClick={handleConnect}
+        />
+      )}
       {/* <InfoCard /> */}
     </div>
   )
